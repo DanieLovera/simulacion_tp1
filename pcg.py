6805f15e30871e377aae8e4b7f0deca558b22a81
Daniel Lovera <daanilo2497@gmail.com>
2024-09-10 02:22:35 -0300

    fix: add 64bit and 32 bit masks to  handle 64bit state and 32bit output as algorithm indicates

--- pcg.py
+++ pcg.py
@@ -1,24 +1,32 @@
 class PCG:
+    __64BIT_MASK = 0xFFFFFFFFFFFFFFFF
+    __32BIT_MASK = 0xFFFFFFFF
+    __5BIT_MASK = 0x1F
+    __MULTIPLIER = 6364136223846793005
+    __INCREMENT = 1442695040888963407
+
     def __init__(self, seed):
-        self.state = seed
-        self.multiplier = 6364136223846793005
-        self.increment = 1442695040888963407
+        self.state = seed + PCG.__INCREMENT
+        self.random()
 
     def random(self):
+        state = self.state
         # LCG
-        self.state = (self.state * self.multiplier + self.increment) & ((1 << 64) - 1)
+        self.state = (state * PCG.__MULTIPLIER + PCG.__INCREMENT) & PCG.__64BIT_MASK
         # Apply xorshift
-        xorshifted = (self.state ^ (self.state >> 18)) >> 27
+        xorshifted = (state ^ (state >> 18)) >> 27
         # Rotate right (RR)
-        rot = self.state >> 59
-        result = PCG.rotate32(xorshifted, rot)
+        rot = state >> 59
+        result = PCG.__rotateright32(xorshifted & PCG.__32BIT_MASK, rot & PCG.__32BIT_MASK)
         return result
 
     def random_normalized(self):
-        return PCG.normalize(self.random())
+        return PCG.__normalize(self.random())
 
-    def rotate32(v, r):
-        return (v >> r) | (v << ((-r) & 31)) & 0xFFFFFFFF
+    @staticmethod
+    def __rotateright32(v, r):
+        return (v >> r | v << (-r & PCG.__5BIT_MASK)) & PCG.__32BIT_MASK
 
-    def normalize(result):
-        return result / 0xFFFFFFFF
+    @staticmethod
+    def __normalize(result):
+        return result / PCG.__32BIT_MASK
