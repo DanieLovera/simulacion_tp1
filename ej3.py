39fb07a74ed9166e81a6d275e97bf70aa5e983f3
Lucas Hemmingsen <lhemmingsen@fi.uba.ar>
2024-09-30 01:30:24 -0300

    Cleaning

--- ej3.py
+++ ej3.py
@@ -5,10 +5,10 @@ import time
 from pcg import PCG
 
 class BrownianSimulator2D:
-    def __init__(self, steps, particles, grid_size, surface_radius, stick_probability):
+    def __init__(self, steps, particles, grid_size, surface_radius, stick_probability, retries):
         seed = int(time.time() * 1_000)
         self.pcg = PCG(seed)
-        self.retries = 10
+        self.retries = retries
         self.particles = particles
         self.grid_size = grid_size
         self.surface_radius = surface_radius
@@ -17,11 +17,11 @@ class BrownianSimulator2D:
         self.particle_positions = self.__initialize_particle_positions()
         self.surface_grid = self.__initialize_surface_grid() 
         
-    # Generates a random position in the grid
+    # Genera una posición aleatoria en la grilla
     def __random_position_in_grid(self):  
         return self.pcg.random() % self.grid_size, self.pcg.random() % self.grid_size
     
-    # Initializes the surface grid, the surface is a circle in the center of the grid
+    # Inicializa la grilla de la superficie
     def __initialize_surface_grid(self):
         surface_grid = np.zeros((self.grid_size, self.grid_size))
         center = self.grid_size // 2
@@ -31,7 +31,7 @@ class BrownianSimulator2D:
                     surface_grid[i, j] = 1
         return surface_grid
     
-    # Returns the initial particle positions, if a position is occupied or is inside the surface, it will try again
+    # Devuelve las posiciones iniciales de las partículas
     def __initialize_particle_positions(self):
         occupied_positions = set()
         surface_center = np.array([self.grid_size // 2, self.grid_size // 2])
@@ -44,13 +44,13 @@ class BrownianSimulator2D:
                 occupied_positions.add(position)        
         return np.array(particles_positions)
     
-    # Returns the next movement of a particle
+    # Devuelve un paso aleatorio de una partícula
     def __random_step(self):
         step_x = int(self.pcg.random_normalized() * 3) - 1 # -1, 0, 1
         step_y = int(self.pcg.random_normalized() * 3) - 1 # -1, 0, 1
         return np.array([step_x, step_y])
     
-    # Updates the position of the particles
+    # Actualiza las posiciones de las partículas
     def update_positions(self):
         occupied_positions = set(map(tuple, self.particle_positions))
         new_positions = []
@@ -79,58 +79,15 @@ class BrownianSimulator2D:
         self.particle_positions = np.array(new_positions)
 
 
-    # Returns True if the position is inside the surface
+    # Devuelve verdadero si la posición está en la superficie
     def __is_on_surface(self, position):
         x, y = position
         return self.surface_grid[x, y] == 1
     
     
-    # Degrades the surface at the given position
+    # Degrada la superficie en la posición dada
     def __degrade_surface(self, position):
         x, y = position
         self.surface_grid[x, y] = 0
-        print(f"Particle stuck at {position}, degrading surface and disappearing.")   
+        print(f"Partícula pegada en ({x},{y}), degradando la superficie y eliminando la partícula...")
         
-
-
-# Parameters
-particles = 1000
-grid_size = 100
-surface_radius = 10
-stick_probability = 0.3
-steps = 100
-
-simulator = BrownianSimulator2D(steps, particles, grid_size, surface_radius, stick_probability)
-
-
-fig, ax = plt.subplots()
-ax.set_xlim(0, grid_size)
-ax.set_ylim(0, grid_size)
-
-# Initialize plot for particles and surface
-particles_plot, = ax.plot([], [], 'bo', markersize=2)
-surface_plot = ax.imshow(simulator.surface_grid, cmap='Greys', origin='lower', extent=[0, grid_size, 0, grid_size])
-
-# Initialization function for the animation
-def init():
-    particles_plot.set_data([], [])
-    surface_plot.set_data(simulator.surface_grid)
-    return particles_plot, surface_plot
-
-# Animation update function
-def update(frame):
-    simulator.update_positions()
-    if len(simulator.particle_positions) > 0:
-        particles_plot.set_data(simulator.particle_positions[:, 0], simulator.particle_positions[:, 1])
-    else:
-        particles_plot.set_data([], [])
-    
-    surface_plot.set_data(simulator.surface_grid)
-    
-    return particles_plot, surface_plot
-
-
-ani = FuncAnimation(fig, update, frames=steps, init_func=init, blit=True, interval=50, repeat=False)
-
-
-plt.show()
